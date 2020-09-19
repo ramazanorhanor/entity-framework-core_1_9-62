368bdce5e6f16f01fec2e57bffe0b45801352df8
ramazanorhanor <ramazanorhanor@hotmail.com>
2020-09-19 07:23:17 +0300

    entity framework 1_9/62

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //CourseApp.Models.Request model;
+          Models.Request model = new Models.Request();
+            model.Name = "Ramazan";
+            model.Email = "ramazanorhanor@hotmail.com";
+            model.Phone = "12345678";
+            model.Message = "Kursa Katılmak istiyorum ";
+            return View(model);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace CourseApp
+{
+    public class Startup
+    {
+        // using Microsoft.Extensions.Configuration;
+        public IConfiguration Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            // projede  mvc middleware projete dahil ettik mvc'yi kullanmak için bunu yazmalıyız
+
+            services.AddDbContext<DataContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DataConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseDeveloperExceptionPage(); // hata olduğunda hata kodlarını görelim
+            app.UseStatusCodePages();  // 404 hatası yada server hatası varsa göstersin
+
+            //wwwroot
+            app.UseStaticFiles(); // wwwroot klasörü css,javascript  ile alakalı
+
+            //package.json -- node_modules dahil etmek için yazılan kod 
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider=new PhysicalFileProvider(Path.Combine( Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath=new PathString("/vendor")
+            });
+
+            // Controller/action/id?
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
